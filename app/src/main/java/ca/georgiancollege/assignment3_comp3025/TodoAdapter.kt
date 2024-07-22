151e6921b3d5e05afafd1bdc54a19554f9d29f06
Sakila-lama <shakila.lama@gmail.com>
2024-07-21 20:34:08 -0400

    updated TodoAdapter file

--- app/src/main/java/ca/georgiancollege/assignment3_comp3025/TodoAdapter.kt
+++ app/src/main/java/ca/georgiancollege/assignment3_comp3025/TodoAdapter.kt
@@ -1,4 +1,49 @@
 package ca.georgiancollege.assignment3_comp3025
 
-class TodoAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ca.georgiancollege.assignment3_comp3025.databinding.ItemTodoBinding
+
+class TodoAdapter
+    (
+    private val todoList: List<Todo>,
+    private val onEditClick: (Todo) -> Unit
+    ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>()
+
+{
+    inner class TodoViewHolder(private val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: Todo) {
+            binding.textViewTaskName.text = todo.name
+            binding.textViewDueDate.text = todo.dueDate
+            binding.switchCompleted.isChecked = todo.isCompleted
+
+            if (todo.dueDate.isNotEmpty()) {
+                binding.textViewDueDate.visibility = android.view.View.VISIBLE
+            } else {
+                binding.textViewDueDate.visibility = android.view.View.GONE
+            }
+
+            binding.buttonEdit.setOnClickListener {
+                onEditClick(todo)
+            }
+
+            binding.switchCompleted.setOnCheckedChangeListener { _, isChecked ->
+                todo.isCompleted = isChecked
+                binding.textViewTaskName.alpha = if (isChecked) 0.5f else 1.0f
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val binding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(todoList[position])
+    }
+
+    override fun getItemCount() = todoList.size
 }
