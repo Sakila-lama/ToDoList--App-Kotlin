fb37cbbf86d9b2e63eaeb8cad963bab4030f17a8
Sakila-lama <shakila.lama@gmail.com>
2024-08-11 19:40:32 -0400

    added the comment header

--- app/src/main/java/ca/georgiancollege/assignment4_comp3025/Todo.kt
+++ app/src/main/java/ca/georgiancollege/assignment4_comp3025/Todo.kt
@@ -2,8 +2,10 @@
  * File Name: Todo.kt
  * Author: Sakila lama
  * StudentID: 200548805
- * Date: July, 21st, 2024
- * App Description: Data class representing a Todo item
+ * Date: August 11th,2024
+ * App Description:This file defines the `Todo` data class, which represents a Todo item
+ *  * with attributes for the task name, notes, due date, completion status, and whether a due date is set.
+ *  * The class implements `Serializable` to allow for easy passing of `Todo` objects between activities.
  * Version: 1.0
  */
 
@@ -12,6 +14,14 @@ package ca.georgiancollege.assignment4_comp3025
 
 import java.io.Serializable
 
+/**
+ * The `Todo` class represents a Todo item in the application.
+ * @property name The name or title of the Todo item.
+ * @property notes Any additional notes related to the Todo item.
+ * @property dueDate The due date for the Todo item as a String.
+ * @property isCompleted A Boolean indicating whether the Todo item is completed.
+ * @property hasDueDate A Boolean indicating whether the Todo item has a due date set.
+ */
 class Todo (
     var name: String = "",
     var notes: String = "",
